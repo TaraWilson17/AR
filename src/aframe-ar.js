8fbf43ea50930edea78c3c5a8939820e6afbfbb6
Tara Wilson <wwtara@hotmail.com>
2018-07-31 10:51:27 -0700

    Updating to latest version with working codes for ceo and desk

--- src/aframe-ar.js
+++ src/aframe-ar.js
@@ -7989,7 +7989,7 @@ ARjs.MarkersAreaUtils.createDefaultMultiMarkerFile = function(trackingBackend){
 	if( trackingBackend === 'artoolkit' ){
 		file.subMarkersControls[0].parameters.type = 'pattern'
 		file.subMarkersControls[0].parameters.patternUrl = absoluteBaseURL + 'examples/marker-training/examples/pattern-files/pattern-hiro.patt'
-		//file.subMarkersControls[0].parameters.patternUrl = 'patts/lol.patt'
+		//file.subMarkersControls[0].parameters.patternUrl = '../patts/test.patt'
 	}else if( trackingBackend === 'aruco' ){
 		file.subMarkersControls[0].parameters.type = 'barcode'
 		file.subMarkersControls[0].parameters.barcodeValue = 1001
@@ -8277,10 +8277,6 @@ AFRAME.registerComponent('arjs-anchor', {
                 arProfile.defaultMarkerParameters.type = 'pattern' 
                 arProfile.defaultMarkerParameters.patternUrl = _this.data.patternUrl; 
 				arProfile.defaultMarkerParameters.markersAreaEnabled = false
-			} else if( _this.data.preset === 'ugh'){
-				markerParameters.type = 'pattern'
-                markerParameters.patternUrl = _this.data.patternUrl; 
-				markerParameters.markersAreaEnabled = false
             } else if( _this.data.preset === 'hiro' ){
 				markerParameters.type = 'pattern'
 				markerParameters.patternUrl = THREEx.ArToolkitContext.baseURL+'examples/marker-training/examples/pattern-files/pattern-hiro.patt'
